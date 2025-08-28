a114d58101016729e4468ff17cf76508a9834c49
Arthur Marcos <arthur_m_martins@estudante.sesisenai.org.br>
2025-08-28 09:15:25 -0300

    o grafico esta funcionando

--- front/graficos.js
+++ front/graficos.js
@@ -8,43 +8,29 @@ document.getElementById('gerarGraficos').addEventListener('click', async () => {
     alert('IDs inválidos');
     return;
   }
-
-  // Limita a no máximo 10 itens
-  const maxRange = Math.min(endId - startId + 1, maxItems);
-
-  // Fetch produtos
   const productPromises = [];
   for (let i = startId; i <= endId && productPromises.length < maxItems; i++) {
-    productPromises.push(fetch(`https://dummyjson.com/products/${i}`).then(res => res.json()));
+    productPromises.push(fetch(`http://localhost:3000/produto/id/${i}`).then(res => res.json()));
   }
   const products = await Promise.all(productPromises);
-
-  const productLabels = products.map(p => p.title);
-  const productStock = products.map(p => p.stock);
-
-  // Fetch usuários
+  const productLabels = products.map(p => p.titulo);
+  const productStock = products.map(p => p.estoque);
   const userPromises = [];
   for (let i = startId; i <= endId && userPromises.length < maxItems; i++) {
-    userPromises.push(fetch(`https://dummyjson.com/users/${i}`).then(res => res.json()));
+    userPromises.push(fetch(`http://localhost:3000/usuario/id/${i}`).then(res => res.json()));
   }
   const users = await Promise.all(userPromises);
-
   const userLabels = users.map(u => `${u.firstName} ${u.lastName}`);
   const userAges = users.map(u => u.age);
-
-  // Criar os gráficos
   renderChart('productChart', 'Produtos x Estoque', productLabels, productStock, 'Estoque');
   renderChart('userChart', 'Usuários x Idade', userLabels, userAges, 'Idade');
 });
 
 function renderChart(canvasId, title, labels, data, label) {
   const ctx = document.getElementById(canvasId).getContext('2d');
-  
-  // Destroi gráfico anterior se existir
   if (window[canvasId + 'Instance']) {
     window[canvasId + 'Instance'].destroy();
   }
-
   window[canvasId + 'Instance'] = new Chart(ctx, {
     type: 'bar',
     data: {
